58435a2de19dea00c6ec018d86f3e3fd03975326
seon54 <seonhyeongpark54@gmail.com>
2024-10-05 16:45:58 +0900

    feat: add a new project

---
+++ src/test/java/com/crowdworks/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdworks.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
